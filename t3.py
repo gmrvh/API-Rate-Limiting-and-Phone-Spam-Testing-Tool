57ecfdc350a4974e16a43e66f4436ccb0bb819d3
gmrvh <140992081+gmrvh@users.noreply.github.com>
2023-08-05 05:17:34 +0300

    Update t3.py

--- t3.py
+++ t3.py
@@ -1,14 +1,16 @@
 #Humbucker bypass
 # 1.0.0  04/08/23  Initial.
 from requests import Request, Session
+from bs4 import BeautifulSoup
 from selenium import webdriver 
+from selenium.webdriver.support.ui import WebDriverWait
+import re
 import cloudscraper
 import chromedriver_autoinstaller 
- 
+import time 
 chromedriver_autoinstaller.install() 
 driver = webdriver.Chrome() 
 
-url = "https://www.dubizzle.com.eg/.humbucker/challenge/js/validate"
 headers = {
     "Host":"auth.dubizzle.com.eg",
       "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
@@ -27,7 +29,7 @@ headers = {
       "TE":"trailers"
 }
 jar = []
-payload = '[{"window":{"innerHeight":1021,"outerHeight":1112,"innerWidth":1598,"outerWidth":1610,"screenX":1825,"screenY":70,"pageXOffset":0,"pageYOffset":0,"devicePixelRation":1},"client":{"width":1581,"height":1021},"screen":{"width":5120,"height":1440,"availWidth":5120,"availHeight":1400,"colorDepth":24,"pixelDepth":24}},"Win32","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",{"ogg":"probably","h264":"probably","webm":"probably"},{"vendor":"Google Inc. (NVIDIA)","renderer":"ANGLE (NVIDIA, NVIDIA GeForce GTX 980 Direct3D11 vs_5_0 ps_5_0)"},["Portable Document Format-application/pdf-pdf","Portable Document Format-text/pdf-pdf"],[{"deviceId":"moIKYAqud7H8cw2g3FyeZjFfMsCKyjbFDqcuW3vrHAE=","kind":"audioinput","label":"","groupId":"cidEOYc7U88AuJ6BNZQs8EwOj4I+4Iz6nEsaV4nPxnI="},{"deviceId":"mPANMaBw2ZttUER5af72tS0QLgeOPMDlv00MCgVF1xU=","kind":"audioinput","label":"","groupId":"cidEOYc7U88AuJ6BNZQs8EwOj4I+4Iz6nEsaV4nPxnI="},{"deviceId":"UVD79xAS7ER1doCfewucP/LMl8JDtKwJSw0DY16AnDw=","kind":"audioinput","label":"","groupId":"I8U46AYijgGl7Q5/ahxLU1SXfVSaKYzXIgibeUq7W9w="},{"deviceId":"cyLWGWw4ry4YnnJQ91HNkidLFbdvZwiACqclVCHBNuE=","kind":"audioinput","label":"","groupId":"Tj6qcoJHzlj37VPmjc5R3gqgYTluWHHL8bOrS7xEPIs="},{"deviceId":"pTeZSfwlGandAhrEQc0HLBfXYFk2NKBKSgxCEcLeXIw=","kind":"audioinput","label":"","groupId":"4cwHEHqZT9eQGWPTpnyr24s4ADPQvuyVFPiz547Fqdw="},{"deviceId":"YBnysqPAtSz5OsS9DdjGzd6XVcndA+hW11OoAorrpXw=","kind":"videoinput","label":"","groupId":"6K83r3mjXng63l7uuXxLg5S9TVkNqf6Rk/DCkXjiLXM="}],false,37,null,{"vibrate":"function vibrate() {\n    [native code]\n}","javaEnabled":"function javaEnabled() {\n    [native code]\n}","getGamepads":"function getGamepads() {\n    [native code]\n}","requestMIDIAccess":"function requestMIDIAccess() {\n    [native code]\n}","mozGetUserMedia":"function mozGetUserMedia() {\n    [native code]\n}","sendBeacon":"function sendBeacon() {\n    [native code]\n}","requestMediaKeySystemAccess":"function requestMediaKeySystemAccess() {\n    [native code]\n}","getAutoplayPolicy":"function getAutoplayPolicy() {\n    [native code]\n}","registerProtocolHandler":"function registerProtocolHandler() {\n    [native code]\n}","taintEnabled":"function taintEnabled() {\n    [native code]\n}","permissions":"function permissions() {\n    [native code]\n}","mimeTypes":"function mimeTypes() {\n    [native code]\n}","plugins":"function plugins() {\n    [native code]\n}","pdfViewerEnabled":"function pdfViewerEnabled() {\n    [native code]\n}","doNotTrack":"function doNotTrack() {\n    [native code]\n}","maxTouchPoints":"function maxTouchPoints() {\n    [native code]\n}","mediaCapabilities":"function mediaCapabilities() {\n    [native code]\n}","oscpu":"function oscpu() {\n    [native code]\n}","vendor":"function vendor() {\n    [native code]\n}","vendorSub":"function vendorSub() {\n    [native code]\n}","productSub":"function productSub() {\n    [native code]\n}","cookieEnabled":"function cookieEnabled() {\n    [native code]\n}","buildID":"function buildID() {\n    [native code]\n}","mediaDevices":"function mediaDevices() {\n    [native code]\n}","serviceWorker":"function serviceWorker() {\n    [native code]\n}","credentials":"function credentials() {\n    [native code]\n}","clipboard":"function clipboard() {\n    [native code]\n}","mediaSession":"function mediaSession() {\n    [native code]\n}","webdriver":"function webdriver() {\n    [native code]\n}","hardwareConcurrency":"function hardwareConcurrency() {\n    [native code]\n}","geolocation":"function geolocation() {\n    [native code]\n}","appCodeName":"function appCodeName() {\n    [native code]\n}","appName":"function appName() {\n    [native code]\n}","appVersion":"function appVersion() {\n    [native code]\n}","platform":"function platform() {\n    [native code]\n}","userAgent":"function userAgent() {\n    [native code]\n}","product":"function product() {\n    [native code]\n}","language":"function language() {\n    [native code]\n}","languages":"function languages() {\n    [native code]\n}","locks":"function locks() {\n    [native code]\n}","onLine":"function onLine() {\n    [native code]\n}","storage":"function storage() {\n    [native code]\n}","constructor":"function Navigator() {\n    [native code]\n}"},["en-US","en"],{"maxTouchPoints":0,"onTouchStart":false,"canHandleTouchEvents":false},{"name":"notifications","state":"prompt"},"function width() {\n    [native code]\n}","-1607895184",{"__nightmare":false,"callPhantom":false,"_phantom":false,"phantom":false,"webdriver":false,"_Selenium_IDE_Recorder":false,"callSelenium":false,"_selenium":false,"__webdriver_script_fn":false,"__driver_evaluate":false,"__webdriver_evaluate":false,"__selenium_evaluate":false,"__fxdriver_evaluate":false,"__driver_unwrapped":false,"__webdriver_unwrapped":false,"__selenium_unwrapped":false,"__fxdriver_unwrapped":false,"__webdriver_script_func":false,"documentSelenium":false,"documentWebdriver":false,"documentDriver":false},{"ogg":"probably","mp3":"maybe","wav":"probably","m4a":"maybe","aac":"maybe"}]'
+payload = '[8,{"vendor":"Google Inc. (NVIDIA)","renderer":"ANGLE (NVIDIA, NVIDIA GeForce GTX 1660 SUPER Direct3D11 vs_5_0 ps_5_0, D3D11)"},false,{"maxTouchPoints":1,"onTouchStart":false,"canHandleTouchEvents":false},"function get width() { [native code] }",{"ogg":"probably","mp3":"probably","wav":"probably","m4a":"","aac":""},{"__nightmare":false,"callPhantom":false,"_phantom":false,"phantom":false,"webdriver":false,"_Selenium_IDE_Recorder":false,"callSelenium":false,"_selenium":false,"__webdriver_script_fn":false,"__driver_evaluate":false,"__webdriver_evaluate":false,"__selenium_evaluate":false,"__fxdriver_evaluate":false,"__driver_unwrapped":false,"__webdriver_unwrapped":false,"__selenium_unwrapped":false,"__fxdriver_unwrapped":false,"__webdriver_script_func":false,"documentSelenium":false,"documentWebdriver":false,"documentDriver":false},["en-US","en"],"422808721","Win32",["-application/pdf-pdf","Portable Document Format-application/x-google-chrome-pdf-pdf"],"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.171 Safari/537.36",{"window":{"innerHeight":720,"outerHeight":814,"innerWidth":856,"outerWidth":872,"screenX":3360,"screenY":304,"pageXOffset":0,"pageYOffset":0,"devicePixelRation":1},"client":{"width":839,"height":703},"screen":{"width":5120,"height":1440,"availWidth":5120,"availHeight":1400,"colorDepth":24,"pixelDepth":24}},null,[{"deviceId":"","kind":"audioinput","label":"","groupId":""},{"deviceId":"","kind":"videoinput","label":"","groupId":""},{"deviceId":"","kind":"audiooutput","label":"","groupId":""}],33,{"ogg":"probably","h264":"","webm":"probably"},{"vendorSub":"function get vendorSub() { [native code] }","productSub":"function get productSub() { [native code] }","vendor":"function get vendor() { [native code] }","maxTouchPoints":"function get maxTouchPoints() { [native code] }","scheduling":"function get scheduling() { [native code] }","userActivation":"function get userActivation() { [native code] }","doNotTrack":"function get doNotTrack() { [native code] }","geolocation":"function get geolocation() { [native code] }","connection":"function get connection() { [native code] }","plugins":"function get plugins() { [native code] }","mimeTypes":"function get mimeTypes() { [native code] }","pdfViewerEnabled":"function get pdfViewerEnabled() { [native code] }","webkitTemporaryStorage":"function get webkitTemporaryStorage() { [native code] }","webkitPersistentStorage":"function get webkitPersistentStorage() { [native code] }","hardwareConcurrency":"function get hardwareConcurrency() { [native code] }","cookieEnabled":"function get cookieEnabled() { [native code] }","appCodeName":"function get appCodeName() { [native code] }","appName":"function get appName() { [native code] }","appVersion":"function get appVersion() { [native code] }","platform":"function get platform() { [native code] }","product":"function get product() { [native code] }","userAgent":"function get userAgent() { [native code] }","language":"function get language() { [native code] }","languages":"function get languages() { [native code] }","onLine":"function get onLine() { [native code] }","webdriver":"function get webdriver() { [native code] }","getGamepads":"function getGamepads() { [native code] }","javaEnabled":"function javaEnabled() { [native code] }","sendBeacon":"function sendBeacon() { [native code] }","vibrate":"function vibrate() { [native code] }","constructor":"function Navigator() { [native code] }","bluetooth":"function get bluetooth() { [native code] }","clipboard":"function get clipboard() { [native code] }","credentials":"function get credentials() { [native code] }","keyboard":"function get keyboard() { [native code] }","managed":"function get managed() { [native code] }","mediaDevices":"function get mediaDevices() { [native code] }","storage":"function get storage() { [native code] }","serviceWorker":"function get serviceWorker() { [native code] }","virtualKeyboard":"function get virtualKeyboard() { [native code] }","wakeLock":"function get wakeLock() { [native code] }","deviceMemory":"function get deviceMemory() { [native code] }","ink":"function get ink() { [native code] }","hid":"function get hid() { [native code] }","locks":"function get locks() { [native code] }","mediaCapabilities":"function get mediaCapabilities() { [native code] }","mediaSession":"function get mediaSession() { [native code] }","serial":"function get serial() { [native code] }","gpu":"function get gpu() { [native code] }","usb":"function get usb() { [native code] }","windowControlsOverlay":"function get windowControlsOverlay() { [native code] }","xr":"function get xr() { [native code] }","userAgentData":"function get userAgentData() { [native code] }","canShare":"function canShare() { [native code] }","share":"function share() { [native code] }","clearAppBadge":"function clearAppBadge() { [native code] }","getBattery":"function getBattery() { [native code] }","getUserMedia":"function getUserMedia() { [native code] }","requestMIDIAccess":"function requestMIDIAccess() { [native code] }","requestMediaKeySystemAccess":"function requestMediaKeySystemAccess() { [native code] }","setAppBadge":"function setAppBadge() { [native code] }","webkitGetUserMedia":"function webkitGetUserMedia() { [native code] }","getInstalledRelatedApps":"function getInstalledRelatedApps() { [native code] }","registerProtocolHandler":"function registerProtocolHandler() { [native code] }","unregisterProtocolHandler":"function unregisterProtocolHandler() { [native code] }"}]'
 
 
 # Initializing a list with two Useragents 
@@ -37,29 +39,83 @@ useragentarray = [
 for i in range(len(useragentarray)): 
 	# Setting user agent iteratively as Chrome 108 and 107 
     driver.execute_cdp_cmd("Network.setUserAgentOverride", {"userAgent": useragentarray[i]}) 
-    driver.get("https://www.dubizzle.com.eg")
-    cookies = driver.get_cookies()
+    cookie_dict = {}
+    try:
+        driver.get("https://www.dubizzle.com.eg") # Get base cookie
+        # Replace 'your_js_file_url_here' with the URL of the JavaScript file you want to wait for
+        js_file_url = "https://www.dubizzle.com.eg/.humbucker/challenge/js/validate"
+
+        # Set up WebDriverWait with a maximum timeout of 30 seconds (you can adjust this value as needed)
+        wait = WebDriverWait(driver, 30)
+
+        # Define a custom expected condition that waits until the specified JavaScript file is loaded
+
+        # Wait until the JavaScript file is loaded before proceeding
+        script = f"return (window.performance.getEntriesByType('resource').find(e => e.name === '{js_file_url}')) !== undefined;"
+        driver.execute_script(script)
+    
+        print("JavaScript file loaded successfully.")
+        # Once the file is loaded, you can continue with your desired actions on the webpage
+        cookies = driver.get_cookies() 
+        for cookie in cookies:
+            cookie_dict[cookie['name']] = cookie['value']
+        print(cookie_dict)
+    except Exception as e:
+        # Handle any exceptions that may occur during the wait or interaction with the page
+        print(f"Error: {e}")
+
+    
     driver.close()
 
-# ... (rest of the code)
+
 
 # Converting Selenium cookies to a format suitable for 'requests'
 cookie_dict = {}
-for cookie in cookies:
-    cookie_dict[cookie['name']] = cookie['value']
+
 
 # Using 'requests' session and adding cookies to the request
 s = Session()
-req = Request('POST', url, data=payload, headers=headers)
+secret_url = "https://www.dubizzle.com.eg/.humbucker/challenge/js/generate/script"
+req = Request('GET', secret_url,)
 s.cookies.update(cookie_dict)  # Add cookies to the session
-
 prepped = req.prepare()
-
-# Use the prepared request with the cookies
 resp = s.send(prepped)
+html_content = resp.text
 
-print(resp.status_code)
-print(resp.text)
-print(resp.cookies)
+def extract_secret_key_from_script(script_content):
+    # Replace this with the logic to extract the secret key from the script_content
+    # For example, you might use regular expressions or string manipulation
+    # to find the secret key within the script_content.
+    # Here, I'll assume that the secret key is within double quotes and contains "eyJ" in it.
+    start_index = script_content.find('"eyJ')
+    end_index = script_content.find('"', start_index + 1)
+    if start_index != -1 and end_index != -1:
+        secret_key = script_content[start_index + 1:end_index]
+        return secret_key
+    return None
+
+soup = BeautifulSoup(html_content, "html.parser")
+# Assuming the secret key is embedded in a <script> tag, you can locate it using its unique identifier or characteristics
+secret_key_script = soup.find("script", {"id": "script_containing_secret_key"})
+
+if html_content:
+    secret_key = extract_secret_key_from_script(html_content)
+    if secret_key:
+        print("Secret Key:", secret_key)
+    else:
+        print("Failed to extract secret key.")
+else:
+    print("Secret key script not found.")
+
+validate_url = "https://www.dubizzle.com.eg/.humbucker/challenge/js/validate"
+cookie_dict.update({"x-hb-co": secret_key}) 
+
+print("Validating secret key...")
+req = Request('POST', validate_url, data=payload, )
+prepped = req.prepare()
+resp = s.send(prepped)
+print("Response:", resp.status_code)
+
+print(cookie_dict)
 
 
